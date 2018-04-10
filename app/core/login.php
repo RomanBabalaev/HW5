7837c26f23e10e21a4f80735f9778287e321cdbf
RomanBabalaev <36676728+RomanBabalaev@users.noreply.github.com>
2018-04-10 23:25:56 +0400

    Add files via upload

---
+++ app/core/login.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "db" => [
+        'host' => 'localhost',
+        'dbname' => 'five_dz',
+        'user' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ],
+];
