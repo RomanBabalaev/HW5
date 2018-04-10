ec222ba903482b4947568ae0fe64528699f42f28
RomanBabalaev <36676728+RomanBabalaev@users.noreply.github.com>
2018-04-10 23:27:23 +0400

    Add files via upload

---
+++ app/models/DBHConnect.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+abstract class DBHConnect
+{
+    protected $DBH;
+
+    function __construct()
+    {
+        $settingsDir = realpath(__DIR__ . '/../core');
+        $configArray = include($settingsDir . DIRECTORY_SEPARATOR . 'config.php');
+        $this->DBH = $configArray['DBH'];
+    }
+}
